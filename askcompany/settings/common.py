bdfde6c24e5b4f148c044064abd3c6ddaa37adc7
somefood <hsj4665@gmail.com>
2020-07-27 23:50:44 +0900

    03 Bootstrap 레이아웃 구현

--- askcompany/settings/common.py
+++ askcompany/settings/common.py
@@ -126,9 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = [
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'askcompany', 'static'),
 ]
-os.path.join(BASE_DIR, 'askcompany', 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
