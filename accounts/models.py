2f6b133eb557b54af7cc9f4129e9bd6bcdc3d083
somefood <hsj4665@gmail.com>
2020-07-28 15:50:43 +0900

    09 프로필 수정 구현과 장고3에서 추가된 TextChoices 활용

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import RegexValidator
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
@@ -6,8 +7,16 @@ from django.template.loader import render_to_string
 
 
 class User(AbstractUser):
+    class GenderChoices(models.TextChoices):
+        MALE = "M", "남성" # 첫번째가 실제 저장되는 값, 두번째는 디스프레이 되는 값
+        FEMALE = "F", "여"
+
     website_url = models.URLField(blank=True)
     bio = models.TextField(blank=True)
+    phone_number = models.CharField(max_length=13, validators=[RegexValidator(r"^010-?[1-9]\d{3}-?\d{4}$")], blank=True)
+    gender = models.CharField(max_length=1, choices=GenderChoices.choices, blank=True)
+    avatar = models.ImageField(blank=True, upload_to="accounts/avatar/%Y/%m/%d",
+                               help_text="48px * 48px 크기의 png/jpg 파일을 업로드해주세요.")
 
     def send_welcome_email(self):
         subject = render_to_string("accounts/welcome_email_subject.txt", {
--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path('login/', views.login, name='login'), # /accounts/login/ => settings.LOGIN_URL
     path('logout/', views.logout, name='logout'),
     path('signup/', views.signup, name='signup'),
-    # path('profile/', views.profile, name='profile'),
-    # path('profile/edit', views.profile_edit, name='profile_edit'),
+    path('edit/', views.profile_edit, name='profile_edit'),
 ]
