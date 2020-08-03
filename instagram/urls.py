988b0cd78fdf2c805b9b28fc454f648bb69ebb71
somefood <hsj4665@gmail.com>
2020-08-03 16:28:54 +0900

    13 유저페이지 구현

--- instagram/urls.py
+++ instagram/urls.py
@@ -6,4 +6,5 @@ app_name = 'instagram'
 urlpatterns = [
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    re_path(r'^(?P<username>[\w.@+-]+)/$', views.user_page, name='user_page'),
 ]
