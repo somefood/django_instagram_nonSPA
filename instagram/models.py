605c8720707ec47e57f22b3a503d5392b4b6d4de
somefood <hsj4665@gmail.com>
2020-08-04 00:22:30 +0900

    19 Timeline 포스팅 목록 조건에 날짜 범위 지정하기

--- instagram/models.py
+++ instagram/models.py
@@ -4,7 +4,16 @@ from django.conf import settings
 from django.urls import reverse
 
 
-class Post(models.Model):
+# 같은 내용 추상클래스 만들기
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Post(BaseModel):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     photo = models.ImageField(blank=True, upload_to='instagram/post/%Y/%m/%d')
     caption = models.CharField(max_length=500)
--- instagram/views.py
+++ instagram/views.py
@@ -1,16 +1,22 @@
+from datetime import timedelta
 from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
+from django.utils import timezone
 from .models import Post
 from .forms import PostForm
 from django.db.models import Q
 
 @login_required
 def index(request):
+    timesince = timezone.now() - timedelta(days=3)
     post_list =  Post.objects.all()\
         .filter(
         Q(author=request.user) | Q(author__in=request.user.following_set.all())
+    )\
+    .filter(
+        created_at__lte=timesince
     )
 
     suggested_user_list = get_user_model().objects.all()\
