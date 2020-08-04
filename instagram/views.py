21d7d67274a74b49ae5f4b608fbcf0d824769892
somefood <hsj4665@gmail.com>
2020-08-05 01:10:56 +0900

    23 장고 위주로 jQuery를 통한 Ajax 댓글 쓰기 구현

--- instagram/views.py
+++ instagram/views.py
@@ -87,6 +87,10 @@ def comment_new(request, post_pk):
             comment.author = request.user
             comment.post = post
             comment.save()
+            if request.is_ajax():
+                return render(request, "instagram/_comment.html", {
+                    "comment": comment,
+                })
             return redirect(comment.post)
     else:
         form = CommentForm()
